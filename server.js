ea02ec726f695864ee7abcd8be31579c65797eed
jedi225 <jedtk@uci.edu>
2018-10-13 13:05:54 -0700

    added console logs for debug

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ var PORT = process.env.PORT || 3000;
 // Middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static("public"));
+app.use(express.static("./public"));
 app.use(
   session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
 );
